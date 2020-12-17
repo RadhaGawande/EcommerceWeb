5f53d386fbd91a5e9540a357e8f940b3dca485df
Bharti Gawande <69391378+RadhaGawande@users.noreply.github.com>
2020-12-17 21:26:35 +0530

    Add files via upload

---
+++ ecom.js
@@ -0,0 +1,31 @@
+/*      Show Menu     */
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+           nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+
+showMenu('nav-toggle','nav-menu')
+
+/*     Remove menu mobile     */
+
+const navlink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+
+    //Active link
+    navlink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+
+    //REmove menu mobile
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+
+}
+
+navlink.forEach(n => n.addEventListener('click',linkAction))
